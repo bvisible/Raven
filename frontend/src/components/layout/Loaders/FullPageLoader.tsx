9e0fda3347184a913fe7ca35228efd5925b2329b
bvisible <info@bvisible.ch>
2024-10-08 09:28:47 +0200

    update trad

--- frontend/src/components/layout/Loaders/FullPageLoader.tsx
+++ frontend/src/components/layout/Loaders/FullPageLoader.tsx
@@ -1,12 +1,13 @@
 import { Loader } from '@/components/common/Loader'
 import { Flex, FlexProps, Text } from '@radix-ui/themes'
 import { clsx } from 'clsx'
-
+import { __ } from '@/utils/translations'
+//// add trad
 type Props = FlexProps & {
     text?: string
 }
 
-export const FullPageLoader = ({ text = "Ravens are finding their way to you...", ...props }: Props) => {
+export const FullPageLoader = ({ text = __("Loading"), ...props }: Props) => {
     return (
         <Flex align='center' width='100%' justify='center' {...props} className={clsx('h-screen', props.className)}>
             <Flex justify='center' align='center' direction='row' gap='4'>
