06a7dbb0a086c7a9c283fbebcc558afdf5d8fac6
Jérémy Christillin <info@bvisible.ch>
2025-04-05 11:52:31 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -70,7 +70,7 @@ export default defineConfig(({ command, mode }) => {
 			emptyOutDir: true,
 			target: "es2015",
 			rollupOptions: {
-				maxParallelFileOps: 1,
+				maxParallelFileOps: 2,
 				onwarn(warning, warn) {
 					if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
 						return
