8070d30248e8d6daef33712eb77ad07f5032fe9b
Jérémy Christillin <jeremy@neoservice.ai>
2026-01-07 18:50:26 +0100

    feat: update frappe-sidebar-react to support collapse in non-fixed mode

--- raven/public/raven/assets/preferences-9RVrxeBb.js
+++
@@ -1,1 +0,0 @@
-import{cs as e}from"./index-Bzdyr4lv.js";const t=e("raven-enter-key-behaviour","send-message",void 0,{getOnInit:!0}),r=e("raven-quick-emojis",["👍","✅","👀","🎉"]);export{t as E,r as Q};
