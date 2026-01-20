89edabfe3ae82f4b386c70174c090535eb9b61cd
Jérémy Christillin <jeremy@neoservice.ai>
2026-01-20 08:19:58 +0100

    fix: add react-native-worklets dependency

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,6 +2,7 @@ module.exports = function (api) {
     api.cache(true);
     const plugins = [];
 
+    plugins.push('react-native-worklets/plugin');
     plugins.push('react-native-reanimated/plugin');
 
     return {
